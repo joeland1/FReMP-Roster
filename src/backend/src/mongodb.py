a70db7d70312e27e84276559c31d1fc5b5678a5a
joeland1 <55666119+joeland1@users.noreply.github.com>
2022-01-11 14:47:31 -0500

    can add + find students

--- src/backend/src/mongodb.py
+++ src/backend/src/mongodb.py
@@ -3,4 +3,9 @@ import pymongo
 def setup():
     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
     print("creating db")
-    mydb = myclient["students"]
+    mydb = myclient["student_databse"]
+    mydb["student_basic"]
+
+def get_student_db():
+    myclient = pymongo.MongoClient("mongodb://mongo:27017/")
+    return myclient.student_database.student_basic
--- src/backend/src/student.py
+++ src/backend/src/student.py
@@ -1,8 +1,25 @@
 from flask import Blueprint, jsonify, request
 import json
+import mongodb
 
 bp = Blueprint('student', __name__, url_prefix='/student')
+# mongoclient[db][table]
+'''
+student
 
+{
+    name: {
+        first:
+        last:
+    }
+    id: 
+    gpa: 
+    classes: [
+        {name: }
+    ]
+}
+
+'''
 @bp.route('/create', endpoint='create', methods=['POST'])
 def create():
     new_students = json.loads(request.headers['students'])
@@ -10,12 +27,27 @@ def create():
     return_students = []
 
     for student in new_students:
-        return_students.append(student['name'])
+        student_col = mongodb.get_student_db()
+        student_col.insert_one({"_id": student['id']})
+        return_students.append(student['id'])
 
     return jsonify({
         'status': 'ok',
         'result': return_students
     }), 200
+
+@bp.route('/get/<netid>', endpoint='get', methods=['GET'])
+def get(netid):
+    if not mongodb.get_student_db().find_one({'_id': netid}):
+        return jsonify({
+            'status': 'error',
+            'desc': 'no student found'
+        })
+    
+    return jsonify({
+        'status': 'ok',
+        'result': 
+    }), 200 
 
 @bp.route('/delete', endpoint='delete', methods=['POST','DELETE'])
 def delete():
