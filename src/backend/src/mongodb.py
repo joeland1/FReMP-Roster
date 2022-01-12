d01cab589c5fa13ac17b8358ac6a859de2fc9e93
joeland1 <55666119+joeland1@users.noreply.github.com>
2022-01-12 17:13:46 -0500

    added functionality to students

--- src/backend/src/mongodb.py
+++ src/backend/src/mongodb.py
@@ -5,7 +5,12 @@ def setup():
     print("creating db")
     mydb = myclient["student_databse"]
     mydb["student_basic"]
+    mydb["detailed_info"]
 
 def get_student_db():
     myclient = pymongo.MongoClient("mongodb://mongo:27017/")
     return myclient.student_database.student_basic
+
+def get_student_extended():
+    myclient = pymongo.MongoClient("mongodb://mongo:27017/")
+    return myclient.student_database.detailed_info
--- src/backend/src/student.py
+++ src/backend/src/student.py
@@ -25,10 +25,23 @@ def create():
     new_students = json.loads(request.headers['students'])
 
     return_students = []
+    
+    student_col = mongodb.get_student_db()
+    extra_info = mongodb.get_student_extended()
 
     for student in new_students:
-        student_col = mongodb.get_student_db()
-        student_col.insert_one({"_id": student['id']})
+        student_col.insert_one({
+            "_id": student['id'],
+            "last_name": student['name']['first'],
+            "first_name": student['name']['last'],
+            "gpa": student['gpa']
+        })
+
+        extra_info.insert_one({
+            "_id": student['id'],
+            "classes": student['classes']
+        })
+
         return_students.append(student['id'])
 
     return jsonify({
@@ -38,21 +51,61 @@ def create():
 
 @bp.route('/get/<netid>', endpoint='get', methods=['GET'])
 def get(netid):
-    if not mongodb.get_student_db().find_one({'_id': netid}):
+    student = mongodb.get_student_db().find_one({'_id': netid})
+    if not student:
         return jsonify({
             'status': 'error',
             'desc': 'no student found'
-        })
+        }), 406
     
     return jsonify({
         'status': 'ok',
-        'result': 
+        'result': student
     }), 200 
 
-@bp.route('/delete', endpoint='delete', methods=['POST','DELETE'])
-def delete():
-    token = "delet func"
+@bp.route('/get/<netid>/all', endpoint='get_plus', methods=['GET'])
+def get_plus(netid):
+    student_basic = mongodb.get_student_db().find_one({'_id': netid})
+    if not student_basic:
+        return jsonify({
+            'status': 'error',
+            'desc': 'no student found'
+        }), 406
+    
+    extended_info = mongodb.get_student_extended().find_one({'_id': netid})
+
     return jsonify({
         'status': 'ok',
-        'result': str(token)
+        'result': student_basic | extended_info
     }), 200
+
+@bp.route('/delete/<netid>', endpoint='delete', methods=['POST','DELETE'])
+def delete(netid):
+    
+    if mongodb.get_student_db().delete_one({'_id': netid}).deleted_count == 0:
+        return jsonify({
+            'status': 'error',
+            'result': 'student not found'
+        }), 406
+
+    mongodb.get_student_extended().delete_one({'_id': netid})
+
+    return jsonify({
+        'status': 'ok',
+        'result': netid
+    }), 200
+
+#this should be done for other stuff but just going to modify classes
+#class:{'name': class_name_here}
+@bp.route('/modify/<netid>', endpoint='modify_add', methods=['POST'])
+def modify_add(netid):
+    mongodb.get_student_extended().update_one({'_id': netid},{'$push':{'classes': json.loads(request.headers['class'])}})
+    return jsonify({
+        'status': 'ok',
+        'change': 'add',
+        'target': request.headers['class']
+    }), 200
+
+@bp.route('/modify/<netid>', endpoint='modify_del', methods=['DELETE'])
+def modify_del(netid):
+    target_class = json.loads(request.headers['class'])
