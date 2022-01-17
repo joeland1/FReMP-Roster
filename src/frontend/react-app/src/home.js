6b1926de519a2c4f6cfc2bf4e59f1551244e59b8
joeland1 <55666119+joeland1@users.noreply.github.com>
2022-01-16 22:46:57 -0500

    update home

--- src/frontend/react-app/src/home.js
+++ src/frontend/react-app/src/home.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 class Home extends React.Component{
     render () {
-        return (<h1>This is the home page!</h1>);
+        return (
+            <div>
+                <h1>This is the home page!</h1>
+                <p>Api port on localhost:81</p>
+                <p>Website on default prot 80</p>
+                <p>Everything is ran through nginx proxy</p>
+            </div>
+        );
     }
 }
 
